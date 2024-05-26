67e1682660e8c49061206af52a22315906153053
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-26 15:39:48 -0300

    feat: make private routes by default

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './jwt-auth.guard'
+
 import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
@@ -23,6 +26,12 @@ import { JwtStrategy } from './jwt.strategy'
       },
     }),
   ],
-  providers: [JwtStrategy],
+  providers: [
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
