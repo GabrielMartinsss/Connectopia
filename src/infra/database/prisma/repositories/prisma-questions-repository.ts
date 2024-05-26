f64721fefb36915ea0909adf0646db6c8f976d15
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-26 08:34:51 -0300

    feat: first conversation between layers (Mapper)

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -3,9 +3,13 @@ import { Injectable } from '@nestjs/common'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { PrismaService } from '../prisma.service'
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(readonly prisma: PrismaService) {}
+
   save(question: Question): Promise<void> {
     throw new Error('Method not implemented.')
   }
@@ -22,8 +26,16 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     throw new Error('Method not implemented.')
   }
 
-  findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!question) return null
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 
   findBySlug(slug: string): Promise<Question | null> {
