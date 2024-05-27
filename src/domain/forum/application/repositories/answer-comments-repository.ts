4339915e1e72f6679661c18385e1520817473124
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-05-27 20:01:08 -0300

    feat: add comment-on-answer controller

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -2,7 +2,7 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
 export abstract class AnswerCommentsRepository {
-  abstract reate(answerComment: AnswerComment): Promise<void>
+  abstract create(answerComment: AnswerComment): Promise<void>
   abstract delete(answerComment: AnswerComment): Promise<void>
   abstract findById(id: string): Promise<AnswerComment | null>
   abstract findManyByAnswerId(
